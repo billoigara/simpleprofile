c9f27c49af60a76fd32b0fb091b2d206fb8bbd99
billoigara <billoigara@gmail.com>
2025-11-11 20:37:40 +0300

    Add files via upload

---
+++ Local variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void myfunction(){
+	//function code block
+	//Local variable declarations and initiaalization
+	int localVar=30;
+	const int age= 25;//constant local variable
+}
+int main(){
+	int localVar=30;
+	const int age=25;
+	return 0;
+}
